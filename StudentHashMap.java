539cd2131ca81e8cd3b611261f683ab06392512c
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-03-04 10:28:35 +0400

    Learn how to create a HashMap in Java and add elements to it.

--- StudentHashMap.java
+++ StudentHashMap.java
@@ -3,44 +3,47 @@ import java.util.Map;
 
 // A class to represent a Student with student ID and name
 class Student {
-    private int studentId;
-    private String studentName;
-
-    // Constructor to initialize student ID and name
-    public Student(int studentId, String studentName) {
-        this.studentId = studentId;
-        this.studentName = studentName;
-    }
-
-    // Getter method for student ID
-    public int getStudentId() {
-        return studentId;
-    }
-
-    // Getter method for student name
-    public String getStudentName() {
-        return studentName;
-    }
+    public Integer studentId;
+    public String studentName;
 }
 
 public class StudentHashMap {
 
-    public  static void  main(String[] args){
+    public static void main(String[] args) {
 
-        HashMap<Integer,Student> studentMap=new HashMap<>();
+        HashMap<Integer, Student> studentMap = new HashMap<>();
         // Add student entries to the HashMap
 
-        studentMap.put(1030,new Student(1030, "Asma"));
-        studentMap.put(1031, new Student(1031,"Ahmed"));
-        studentMap.put(1032, new Student(1032,"Balqees"));
-        studentMap.put(1033, new Student(1033,"Ali"));
-        studentMap.put(1034, new Student(1034,"Hamed"));
+        Student student1 = new Student();
+        student1.studentId = 1030;
+        student1.studentName = "Asma";
+        studentMap.put(1030, student1);
+
+        Student student2 = new Student();
+        student2.studentId = 1031;
+        student2.studentName = "Ahmed";
+        studentMap.put(1031, student2);
+
+        Student student3 = new Student();
+        student3.studentId = 1032;
+        student3.studentName = "Balqees";
+        studentMap.put(1032, student3);
+
+        Student student4 = new Student();
+        student4.studentId = 1033;
+        student4.studentName = "Ali";
+        studentMap.put(1033, student4);
+
+        Student student5 = new Student();
+        student5.studentId = 1034;
+        student5.studentName = "Hamed";
+        studentMap.put(1034, student5);
 
         System.out.println("Student HashMap:");//Print the entire HashMap to verify the entries
-        // loop for printing all the hash map have recoded
-        for (Map.Entry<Integer, Student> record: studentMap.entrySet()) {
+        // loop for printing all the hash map have recorded
+        for (Map.Entry<Integer, Student> record : studentMap.entrySet()) {
             Student student = record.getValue();
-            System.out.println("Student ID: " + student.getStudentId() + ", Name: " + student.getStudentName());
+            System.out.println("Student ID: " + student.studentId + ", Name: " + student.studentName);
         }
 
     }
